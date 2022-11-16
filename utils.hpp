903e6195bf35129626d119af60b24aa5d457b58a
黄元镭 <huangyuanlei@hotmail.com>
2022-11-16 17:26:51 +0800

    Update

--- utils.hpp
+++ utils.hpp
@@ -25,7 +25,9 @@
 #endif
 
 #ifdef CPPHTTPLIB_HTTPLIB_SUPPORT
-/* macOS CMakeLists.txt
+/*
+ * CMakeLists.txt Darwin
+ *
  * include_directories(${CMAKE_CURRENT_SOURCE_DIR}/../common/github.com/unwitting.life/cpp-utils/OpenSSL/3.0.6+/darwin64-x86_64-cc/include)
  * link_directories(${CMAKE_CURRENT_SOURCE_DIR}/../common/github.com/unwitting.life/cpp-utils/OpenSSL/3.0.6+/darwin64-x86_64-cc/lib)
  * add_executable(${PROJECT_NAME} main.cpp)
